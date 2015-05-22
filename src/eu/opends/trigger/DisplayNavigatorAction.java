41102fc20707996047472096fa72def019925747
Karasion <davillmart@gmail.com>
2015-05-22 18:19:56 +0900

    add DisplayNavigatorAction.java for Navigation

--- src/eu/opends/trigger/DisplayNavigatorAction.java
+++ src/eu/opends/trigger/DisplayNavigatorAction.java
@@ -1,22 +1,23 @@
 package eu.opends.trigger;
 
-import eu.opends.hud.HudDisplay;
+import eu.opends.hud.HUDManagement;
 
-
+//Im gisung, Hong sunghyeon
 public class DisplayNavigatorAction extends TriggerAction {
 	String naviType;
+	String distance;
 	
-	public DisplayNavigatorAction(String naviType)
+	public DisplayNavigatorAction(String naviType, String distance)
 	{
 		super();
 		this.naviType = naviType;
+		this.distance = distance;
 	}
 	
 	// override
 	protected void execute()
 	{
-		System.out.println(naviType);
-		HudDisplay.setNaviType(naviType);
+		HUDManagement.setNaviType(naviType, distance);
 	}
 	// override
 	public String toString()
